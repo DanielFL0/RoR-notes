0dcc00b66f52dbf887d0cc02f46d00df2ae4177b
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-09-09 16:12:07 -0500

    Modify assignment_2.rb

--- Ruby/assignment_2.rb
+++ Ruby/assignment_2.rb
@@ -5,14 +5,35 @@
 # Multiplication *
 # Division /
 # Modulus %
+
+def add(first_num, second_num)
+  first_num.to_f + second_num.to_f
+end
+
+def subtract(first_num, second_num)
+  first_num.to_f - second_num.to_f
+end
+
+def multiply(first_num, second_num)
+  first_num.to_f * second_num.to_f
+end
+
+def divide(first_num, second_num)
+  first_num.to_f / second_num.to_f
+end
+
+def modulus(first_num, second_num)
+  first_num.to_f % second_num.to_f
+end
+
 puts "Simple calculator"
 puts "-" * 20 # Can also be written as 20.times { print "-" }
 puts "Enter the first number: "
 num_1 = gets.chomp
 puts "Enter the second number: "
 num_2 = gets.chomp
-puts "The first number #{num_1} added by the second number #{num_2} is equal to #{num_1.to_f + num_2.to_f}"
-puts "The first number #{num_1} subtracted by the second number #{num_2} is equal to #{num_1.to_f - num_2.to_f}"
-puts "The first number #{num_1} multiplied by the second number #{num_2} is equal to #{num_1.to_f * num_2.to_f}"
-puts "The first number #{num_1} divided by the second number #{num_2} is equal to #{num_1.to_f / num_2.to_f}"
-puts "The remainder of the first number #{num_1} divided by the second number #{num_2} is #{num_1.to_f % num_2.to_f}"
+puts "The first number #{num_1} added by the second number #{num_2} is equal to #{add(num_1, num_2)}"
+puts "The first number #{num_1} subtracted by the second number #{num_2} is equal to #{subtract(num_1, num_2)}"
+puts "The first number #{num_1} multiplied by the second number #{num_2} is equal to #{multiply(num_1, num_2)}"
+puts "The first number #{num_1} divided by the second number #{num_2} is equal to #{divide(num_1, num_2)}"
+puts "The remainder of the first number #{num_1} divided by the second number #{num_2} is #{modulus(num_1, num_2)}"
