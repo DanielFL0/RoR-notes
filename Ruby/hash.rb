56c31eccd1c149ced876fb7a5178c2a5328c874f
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-09-17 18:04:14 -0500

    Add hash.rb

---
+++ Ruby/hash.rb
@@ -0,0 +1,26 @@
+module Hash
+    require 'bcrypt'
+    def Hash.create_hash_digest(password)
+        BCrypt::Password.create(password)
+    end
+
+    def Hash.verify_hash_digest(password)
+        BCrypt::Password.new(password)
+    end
+
+    def Hash.create_secure_users(list_of_users)
+        list_of_users.each do |user|
+            user[:password] = create_hash_digest(user[:password])
+        end
+        list_of_users # returns list_of_users
+    end
+
+    def Hash.authenticate_user(username, password, list_of_users)
+        list_of_users.each do |user|
+            if user[:username] == username && verify_hash_digest(user[:password]) == password
+                return user
+            end
+        end
+        "Credentials were not correct"
+    end
+end
