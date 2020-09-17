c3f9bd0c370b9b84487e17f8008b0d912613afc1
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-09-17 17:49:47 -0500

    Modify bcrypt_module.rb

--- Ruby/bcrypt_module.rb
+++ Ruby/bcrypt_module.rb
@@ -2,7 +2,13 @@ require 'bcrypt'
 
 my_password = BCrypt::Password.create("My password")
 
-puts my_password
-puts my_password.version
-puts my_password == "My password"
-puts my_password == "Not my password"
+puts my_password # prints $2a$12$mqYuMzrugvn2e9cDtJ0yDe8npdBXDAfEC2U43HtGR.h2f5AU1UcoS
+puts my_password.version # prints 2a
+puts my_password == "My password" # prints true
+puts my_password == "Not my password" # prints false
+
+unhashed_password = BCrypt::Password.new("$2a$12$mqYuMzrugvn2e9cDtJ0yDe8npdBXDAfEC2U43HtGR.h2f5AU1UcoS")
+
+puts unhashed_password
+puts unhashed_password == "My password" # prints true
+puts unhashed_password == "Not my password" # prints false
